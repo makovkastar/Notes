f7fc34b2993a32d1e7552033a77a2a5c8401c942
Oleksandr Melnykov <makovkastar@gmail.com>
2017-12-16 22:40:59 +0100

    Enable the strict mode in debug builds.

--- app/src/main/java/com/melnykov/notes/NotesApplication.kt
+++ app/src/main/java/com/melnykov/notes/NotesApplication.kt
@@ -1,13 +1,21 @@
 package com.melnykov.notes
 
 import android.app.Application
+import android.os.StrictMode
 import com.facebook.stetho.Stetho
 
 class NotesApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        enableStrictMode()
         initStetho()
+    }
+
+    private fun enableStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.enableDefaults()
+        }
     }
 
     private fun initStetho() {
