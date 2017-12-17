f03526721dda5c59475e987561c9fdf729281ed5
Oleksandr Melnykov <makovkastar@gmail.com>
2017-12-17 22:00:19 +0100

    Implement the layout for the note list screen.

--- app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
+++ app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
@@ -2,6 +2,7 @@ package com.melnykov.notes.ui.list
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import com.melnykov.notes.R
 
 class NoteListActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.note_list, menu)
+        return true
     }
 }
