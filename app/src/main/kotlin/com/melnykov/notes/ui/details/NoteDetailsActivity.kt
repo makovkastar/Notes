ce7969dab443e47594bf85d583389b640f3f06d5
Oleksandr Melnykov <makovkastar@gmail.com>
2017-12-17 22:58:09 +0100

    Implement the layout for the note details screen.

--- app/src/main/kotlin/com/melnykov/notes/ui/details/NoteDetailsActivity.kt
+++ app/src/main/kotlin/com/melnykov/notes/ui/details/NoteDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.melnykov.notes.ui.details
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.melnykov.notes.R
 
 class NoteDetailsActivity : AppCompatActivity() {
@@ -9,5 +10,20 @@ class NoteDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_details)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
+++ app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
@@ -1,15 +1,23 @@
 package com.melnykov.notes.ui.list
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import com.melnykov.notes.R
+import com.melnykov.notes.ui.details.NoteDetailsActivity
+import kotlinx.android.synthetic.main.activity_note_list.*
+
 
 class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        text_take_note.setOnClickListener({
+            startActivity(Intent(this, NoteDetailsActivity::class.java))
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
