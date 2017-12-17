8a8a145ce0cfc160caf3192b57cb660420d88d5e
Oleksandr Melnykov <makovkastar@gmail.com>
2017-12-17 23:50:13 +0100

    Implement the layout for the search screen.

--- app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
+++ app/src/main/kotlin/com/melnykov/notes/ui/list/NoteListActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuItem
 import com.melnykov.notes.R
 import com.melnykov.notes.ui.details.NoteDetailsActivity
+import com.melnykov.notes.ui.search.SearchActivity
 import kotlinx.android.synthetic.main.activity_note_list.*
 
 
@@ -23,5 +25,15 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.note_list, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_search -> {
+                startActivity(Intent(this, SearchActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/kotlin/com/melnykov/notes/ui/search/SearchActivity.kt
+++ app/src/main/kotlin/com/melnykov/notes/ui/search/SearchActivity.kt
@@ -3,11 +3,17 @@ package com.melnykov.notes.ui.search
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.melnykov.notes.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
